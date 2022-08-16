3f0d3c0cb7f36ca39031638a3e171d2e4ed8bccf
ahmed imran <ahmed.eng2709@gmail.com>
2022-08-16 20:46:57 +0500

    change name

--- server.mjs
+++ server.mjs
@@ -1,142 +1,120 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
 const app = express();
-app.use(express.json())
-app.use(cors())
-const port =process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
+const port = process.env.PORT || 3000;
 
-let User =[];
+let users = [];
 
 function randomNumber() {
   return Math.floor(Math.random() * 100000000000);
 }
 app.post("/user", (req, res) => {
+  console.log(req.body);
+  let newUser = {
+    id: randomNumber(),
+    fullname: req.body.fullname,
+    username: req.body.username,
+    password: req.body.password,
+  };
+  users.push(newUser);
+  res.status(201).send("user is created"); // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#successful_responses
+});
 
-  console.log(req.body)
-let newUser = {
-  id: randomNumber(),
-  fullname: req.body.fullname,
-  username: req.body.username,
-  password: req.body.password
-}
-users.push(newUser);
-res.status(201).send("user is created"); // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#successful_responses
-})
-
-app.get("/user/:userId", (req, res) => { // get single user
+app.get("/user/:userId", (req, res) => {
+  // get single user
 
   let userId = req.params.userId;
   let isFound = false;
 
   for (let i = 0; i < users.length; i++) {
-      if (users[i].id == userId) {
-          res.send(users[i]);
-          isFound = true;
-          break;
-      }
+    if (users[i].id == userId) {
+      res.send(users[i]);
+      isFound = true;
+      break;
+    }
   }
   if (!isFound) {
-      res.status(204).send("user not found");
+    res.status(204).send("user not found");
   }
-})
+});
 
-app.get("/users", (req, res) => { // get all users
+app.get("/users", (req, res) => {
+  // get all users
   res.send(users);
-})
+});
 
-app.put("/user/:userId", (req, res) => { // to modify single user
+app.put("/user/:userId", (req, res) => {
+  // to modify single user
   res.send("your user is modified");
 
   let userId = req.params.userId;
-  let userIndex = -1
+  let userIndex = -1;
 
   for (let i = 0; i < users.length; i++) {
-      if (users[i].id == userId) {
-          userIndex = i;
-          break;
-      }
+    if (users[i].id == userId) {
+      userIndex = i;
+      break;
+    }
   }
   if (userIndex === -1) {
     res.status(204).send("user not found");
-} else {
-
+  } else {
     if (req.body.fullname) {
-        users[userIndex].fullname = req.body.fullname
-    };
+      users[userIndex].fullname = req.body.fullname;
+    }
     if (req.body.username) {
-        users[userIndex].username = req.body.username
-    };
+      users[userIndex].username = req.body.username;
+    }
     if (req.body.password) {
-        users[userIndex].password = req.body.password
-    };
+      users[userIndex].password = req.body.password;
+    }
 
     res.send(users[userIndex]);
-}
-})
+  }
+});
 
-app.delete("/user/:userId", (req, res) => { // delete single user
+app.delete("/user/:userId", (req, res) => {
+  // delete single user
 
   let userId = req.params.userId;
-  let userIndex = -1
+  let userIndex = -1;
 
   for (let i = 0; i < users.length; i++) {
-      if (users[i].id == userId) {
-          userIndex = i;
-          break;
-      }
+    if (users[i].id == userId) {
+      userIndex = i;
+      break;
+    }
   }
 
   if (userIndex === -1) {
-
-      res.status(204)
-      res.send("user not found");
-
+    res.status(204);
+    res.send("user not found");
   } else {
-
-      users.splice(userIndex, 1);
-      res.send("user is deleted");
+    users.splice(userIndex, 1);
+    res.send("user is deleted");
   }
-})
+});
 
-app.delete("/users", (req, res) => { // delete all users
-    
+app.delete("/users", (req, res) => {
+  // delete all users
+
   users = [];
-  
+
   res.send("all users deleted");
-})
+});
 
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   console.log("aik request server per i");
-  res.send('Hello World!')
-})
+  res.send("Hello World!");
+});
 
-app.get('/profile', (req, res) => {
+app.get("/profile", (req, res) => {
   console.log("aik request server per i");
-  res.send('this is a profile!')
-})
+  res.send("this is a profile!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  console.log(`Example app listening on port ${port}`);
+});
